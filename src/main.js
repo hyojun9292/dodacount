f6197517cad61615b75cf5e50e2f560e9602f51d
yoohyojun92 <78941801+yoohyojun92@users.noreply.github.com>
2022-03-26 14:34:01 +0900

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Vuex from 'vuex'
-
-createApp(App).use(store).use(router).use(Vuex).mount('#app')
